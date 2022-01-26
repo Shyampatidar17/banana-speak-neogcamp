cee3dd55b083ad023df7cac171f62838d0bf3463
Shyampatidar17 <shyampatidar171995@gmail.com>
2022-01-26 14:19:35 +0530

    update-event

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-console.log("hello! script is working")
+// console.log("hello! script is working")
 
 // input
-var username = prompt("give me username")
+// var username = prompt("give me username")
 
 // processing
-var welcomeMessage="this script work!! " + username;
+// var welcomeMessage="this script work!! " + username;
 
 // output
-alert(welcomeMessage);
+// alert(welcomeMessage);
+
+var btnTranslate = document.querySelector("#btn-translate");
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+    console.log("clicked!")
+})
+
+console.log(btnTranslate);
