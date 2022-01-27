ee95e071ef994a428ddd2cad4d5c6c810dcd9580
Shyampatidar17 <shyampatidar171995@gmail.com>
2022-01-27 17:25:39 +0530

    add server url neog camp+ error ntification show

--- app.js
+++ app.js
@@ -13,14 +13,33 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandlerURL(error){
+    console.log("error occured" , error)
+    alert("something wrong with server! try again after some time")
+}
+
 // outputDiv.innerText= "shyam patidar";
 
 // console.log(outputDiv)
 
 function clicHandler() {
-    outputDiv.innerText = "amnbmnabm " + txtInput.value;
+    // outputDiv.innerText = "amnbmnabm " + txtInput.value;
     // console.log("clicked!");
     // console.log("input" , txtInput.value);
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated))
+    .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clicHandler)
